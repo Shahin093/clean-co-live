279c070cf73354a29ae06afec48b8fb6a999d61f
Shidul Islam <sishahin093@gmail.com>
2022-05-15 10:44:28 +0600

    completing added router and navbar and landing page setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import { publicRoute } from './routes/publicRoute';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar>
+        <Routes>
+          {/* <Route path='/home' element={<Home></Home>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
+          <Route path='/services' element={<Services></Services>}></Route>
+          <Route path='/contact' element={<Contact></Contact>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route> */}
+          {/* <PublicRoutes></PublicRoutes> */}
+          {
+            publicRoute.map(route => <Route path={route.path} element={<route.Component />}></Route>)
+          }
+        </Routes>
+
+
+      </Navbar>
+
     </div>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Landing from './landing';
+// import Landing from './landing';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This is Home</h2>
+            <Landing></Landing>
+        </div>
+    );
+};
+
+export default Home;
